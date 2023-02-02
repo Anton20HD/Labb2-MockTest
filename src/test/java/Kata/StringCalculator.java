1923800ae890d1e5f7a9bc920b77b0e7aa3ad184
Anton <anton.haglund@iths.se>
2023-02-02 12:50:32 +0100

    Added delimiter

--- src/test/java/Kata/StringCalculator.java
+++ src/test/java/Kata/StringCalculator.java
@@ -9,19 +9,33 @@ public class StringCalculator {
             return 0;
 
 
-        }
-        String[] numberArr = numbers.split("[,\n]");
-        int sum = 0;
-        for (var number : numberArr) {
-            sum += Integer.parseInt(number);
+    }
+
+           String delimiter = "[,\n]";
+            if (CustomDelimiterBeingUsed(numbers)) {
+                int firstNewIndex = numbers.indexOf("\n");
+                delimiter = numbers.substring(2, firstNewIndex);
+                numbers = numbers.substring(firstNewIndex + 1);
+
+            }
+
+            String[] numberArr = numbers.split(delimiter);
+            int sum = 0;
+            for (String number : numberArr) {
+                sum += Integer.parseInt(number);
 
 
 //        String[] numberArr = numbers.split(",");
 //        return Arrays.stream(numberArr)
 //                .mapToInt(Integer::parseInt)
 //                .sum();
+            }
+            return sum;
         }
-        return sum;
+
+
+    private static boolean CustomDelimiterBeingUsed(String numbers) {
+        return numbers.startsWith("//");
     }
 
 }
